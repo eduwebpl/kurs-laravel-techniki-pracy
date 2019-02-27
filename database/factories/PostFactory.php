e1e63fa589069c288a3796264b9169429050d252
Piotr Palarz <piotr@depalage.com>
2019-02-27 12:30:11 +0100

    11-finish

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Post::class, function (Faker $faker) {
+    return [
+        'title' => $faker->sentence(5),
+        'content' => $faker->paragraph(5),
+        'date' => now(),
+        'type' => 'text'
+    ];
+});
+
+$factory->state(App\Post::class, 'image', function (Faker $faker) {
+    return [
+        'content' => null,
+        'type' => 'photo',
+        'image' => $faker->imageUrl(1200, 800)
+    ];
+});
