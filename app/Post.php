312b9ce4cde4152bdd7994d15a022b166462f2a4
Piotr Palarz <piotr@depalage.com>
2019-02-27 14:12:30 +0100

    16-finish

--- app/Post.php
+++ app/Post.php
@@ -4,7 +4,15 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Support\Str;
+
 class Post extends Model
 {
     protected $guarded = [];
+
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
 }
