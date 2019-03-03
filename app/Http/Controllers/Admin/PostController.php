82785537ecfd1a4ef739fb13cfbe061750caa008
Piotr Palarz <piotr@depalage.com>
2019-03-03 11:45:59 +0100

    40-finish

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -12,6 +12,12 @@ use Illuminate\Support\Facades\Storage;
 
 class PostController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+        $this->middleware('can:manage-posts');
+    }
+
     protected function validator($data)
     {
         return Validator::make($data, [
