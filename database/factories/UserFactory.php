19f534c3553614fdc1ecb989e94b7b00eda79352
Piotr Palarz <piotr@depalage.com>
2019-03-03 11:59:28 +0100

    42-finish

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,3 +24,11 @@ $factory->define(User::class, function (Faker $faker) {
         'remember_token' => Str::random(10),
     ];
 });
+
+$factory->state(User::class, 'admin', function (Faker $faker) {
+    return [
+        'name' => 'Piotr Palarz',
+        'email' => 'piotr@eduweb.pl',
+        'password' => bcrypt('test')
+    ];
+});
