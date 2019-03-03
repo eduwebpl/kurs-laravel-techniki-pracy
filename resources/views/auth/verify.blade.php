f62790a3744a78c7cb115b6ac36f5930a37adb08
Piotr Palarz <piotr@depalage.com>
2019-03-03 09:41:17 +0100

    30-finish

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,24 +1,14 @@
-@extends('layouts.app')
+@extends('layouts.master')
+@section('title', 'Verify Your Email')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+<div class="wrapper">
+    <div class="rte">
+        <h1>Verify Your Email</h1>
+    </div>
 
-                <div class="card-body">
-                    @if (session('resent'))
-                        <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
-                        </div>
-                    @endif
-
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }}, <a href="{{ route('verification.resend') }}">{{ __('click here to request another') }}</a>.
-                </div>
-            </div>
-        </div>
+    <div class="rte mt">
+        <p>Didn't get an email? <a href="{{ route('verification.resend') }}">Request another.</a></p>
     </div>
 </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/about-me', function () {
     return view('pages.about');
 })->name('about');
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
 Route::get('/account/register', 'Auth\RegisterController@showRegistrationForm')->name('register');
 Route::post('/account/register', 'Auth\RegisterController@register');
@@ -29,3 +29,6 @@ Route::get('/password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm
 Route::post('/password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
 Route::get('/password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('/password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
+Route::get('/email/verify/{id}', 'Auth\VerificationController@verify')->name('verification.verify');
+Route::get('/email/verify', 'Auth\VerificationController@show')->name('verification.notice');
+Route::get('/email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
